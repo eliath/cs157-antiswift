0623b4d9b31e27a90ff406759816f8b5800f2178
eliath <eliasmartinezcohen@gmail.com>
2014-09-05 16:59:24 -0400

    v0.9

---
+++ antiswift.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name 			cs157 Antiswift
+// @namespace		http://eliasmartinezcohen.com/
+// @include     	http://cs.brown.edu/courses/csci1570/*
+// @require			http://code.jquery.com/jquery-latest.min.js
+// @run-at			document-start
+//
+// @version    0.9.1
+// 
+// ==/UserScript==
+
+// Cover up TSwift shit
+$('html').css('background', '#242424');
+
+// Bind some shortcuts ;)
+$(document).keypress(function(event) {
+	if (event.which >= 49 && event.which <= 54) //1
+		window.location = $('#navigation li:nth-child('+ (event.which - 48) +') a').attr('href');
+}).ready(function(event) {
+	//Display shortcut numbers when DOM loads
+	var styles = "font-size: .7em; vertical-align: super; margin-right: 5px;";
+	$('#navigation li a').each(function(i) {
+		$(this).prepend('<span style="'+ styles+'">' + (i+1) + '</span>');
+	});
+});
